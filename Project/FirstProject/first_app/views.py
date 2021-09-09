318fc32744b3524f8da80703178413194e1635eb
Abhijit Shete <abhijitshete13@gmail.com>
2021-09-09 13:31:57 +0530

    Section-16 122

--- Project/FirstProject/first_app/views.py
+++ Project/FirstProject/first_app/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    my_dict = {'insert_me':"Hello now i'm from first app"}
+    return render(request, 'first_app/index.html', context=my_dict)
