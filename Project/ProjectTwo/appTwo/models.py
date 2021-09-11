67200591afb8a062e454e3cf82a0101c471703a4
Abhijit Shete <abhijitshete13@gmail.com>
2021-09-11 16:54:21 +0530

    Section-17

---
+++ Project/ProjectTwo/appTwo/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class MyUser(models.Model):
+    first_name = models.CharField(max_length=125)
+    last_name = models.CharField(max_length=125)
+    email = models.EmailField(max_length=250,unique=True)
+
